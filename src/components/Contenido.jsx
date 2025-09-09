581af9dc4df762c7488843ceb15fd4422c2bfe1f
AndreaC05 <aguirreandrea457@gmail.com>
2025-09-09 16:52:12 -0500

    avance

--- src/components/Contenido.jsx
+++ src/components/Contenido.jsx
@@ -4,7 +4,19 @@ export default function Contenido() {
     return(
         <>
         <div className="container_texto_invitacion">
-            
+            <div class="svg-container">
+                <svg width="400" height="150" viewBox="0 0 400 150">
+                    <defs>
+                        <path id="curve" d="M 20 80 Q 200 20 380 80" stroke="none" fill="none"/>
+                    </defs>
+                    <text class="svg-text">
+                        <textPath href="#curve">¡Ven a celebrar!</textPath>
+                    </text>
+                </svg>
+            </div>
+            <div className="img_logo">
+                <img src="https://res.cloudinary.com/dnao6nouz/image/upload/v1757452190/descarga_1_l5tlnh.jpg" alt="" />
+            </div>
         </div>
         </>
     );
