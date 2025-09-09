2c805462d6998eb14ff2073153dd4bab253f5a68
AndreaC05 <aguirreandrea457@gmail.com>
2025-09-09 12:46:18 -0500

    avance

---
+++ src/components/Contenido.jsx
@@ -0,0 +1,11 @@
+import "../style/Contenido.css";
+
+export default function Contenido() {
+    return(
+        <>
+        <div className="container_texto_invitacion">
+            
+        </div>
+        </>
+    );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,31 @@
+import "../style/Home.css";
+import Contenido from "../components/Contenido";
+
 export default function Home() {
-    return(
-        <>
-        
-        </>
+    return (
+        <div className="container_home">
+            {/* Video difuso de fondo */}
+            <video 
+                className="video-background"
+                src="https://res.cloudinary.com/dnao6nouz/video/upload/v1757038592/PinDown.io__zzzgalina_1757038570_lz5nov.mp4"
+                autoPlay
+                muted
+                loop
+                playsInline
+            ></video>
+
+            {/* Video principal normal */}
+            <video 
+                className="video-main"
+                src="https://res.cloudinary.com/dnao6nouz/video/upload/v1757038592/PinDown.io__zzzgalina_1757038570_lz5nov.mp4"
+                autoPlay
+                muted
+                loop
+                playsInline
+            ></video>
+            <div className="contenido_text">
+                <Contenido />
+            </div>
+        </div>
     );
 }
